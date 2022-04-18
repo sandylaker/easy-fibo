bfbde18c8cd2e6df46590ea6107521a047528037
Yawei Li <yawei.li@tum.de>
2022-04-18 16:38:22 +0200

    add docs

--- easy_fibo/fibonacci.py
+++ easy_fibo/fibonacci.py
@@ -1,4 +1,12 @@
 def fibonacci(n: int) -> int:
+    """Compute the fibonacci number.
+
+    Args:
+        n: Index of the fibonacci number.
+
+    Returns:
+        The n-th fibonacci number.
+    """
     if n < 0:
         raise ValueError(f"n must be non-negative, but got {n}")
     if n == 0:
